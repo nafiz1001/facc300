5b1a0074866ab428761c1b511b866f7648e46984
nafiz1001 <nafiz.islam1001@gmail.com>
2022-11-10 06:37:01 -0500

    use defrecord and defprotocol

--- src/main/facc/frontend/app.cljs
+++ src/main/facc/frontend/app.cljs
@@ -7,14 +7,24 @@
   (defn reciprocal [f] #(/ 1 (f %1 %2)))
   
   (defn present-value [i n] (pow (/ 1 (+ 1 i)) n))
+  (def compound-amount (reciprocal present-value))
   
   (defn geometric-series-present-value [i g n]
     (cond
-      (neg-int? n) (/ 1 (- i g))
+      (infinite? n) (/ 1 (- i g))
       (= i g) (/ n (+ i g))
-      :else (/ (- 1 (pow (/ (+ 1 g) (+ 1 i)) n)) (- i g))))
+      :else (/ (- 1 (pow (/ (+ 1 g) (+ 1 i)) n)) (- i g)))) 
   
-  (rdom/render [:div "hello space"] (js/document.getElementById "app")))
+  (defprotocol Monetary-Flow
+    (at [n] "get monetary flow at period n"))
+  
+  (defrecord Geometric-Series [N i Io g]
+    Monetary-Flow
+    (at [n] (if (or (<= n N) (infinite? N)) (* Io (compound-amount g n)) 0))
+    (npv [n] (* Io (if (<= n N) (- (geometric-series-present-value i g N) (geometric-series-present-value i g n)) 0))))
+  
+  (rdom/render [:div "hello space"] (js/document.getElementById "app"))
+  )
 
 (defn init []
   (println "Hello World")
