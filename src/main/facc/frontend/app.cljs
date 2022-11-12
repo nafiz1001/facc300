a6cacf6f0273f5d57892b6183ad399c90760f4bb
nafiz1001 <nafiz.islam1001@gmail.com>
2022-11-11 20:47:43 -0500

    back to functional

--- src/main/facc/frontend/app.cljs
+++ src/main/facc/frontend/app.cljs
@@ -6,22 +6,26 @@
   
   (defn reciprocal [f] #(/ 1 (f %1 %2)))
   
-  (defn present-value [i n] (pow (/ 1 (+ 1 i)) n))
-  (def compound-amount (reciprocal present-value))
+  (defn compound-amount [i n] (pow (+ 1 i) n))
+  (def present-value (reciprocal compound-amount))
   
+  (defn series-compound-amount [i n]
+    (/ (- (compound-amount i n) 1) i))
+  (def sinking-fund (reciprocal series-compound-amount))
+  
+  (defn series-present-value [i n]
+    (if (infinite? n)
+      (/ 1 i)
+      (/ (- 1 (present-value i n)) i)))
+  (def capital-recovery (reciprocal series-present-value))
+  
+  (defn uniform-gradient-series [i n]
+    (- (/ 1 i) (/ n (- (compound-amount i n) 1))))
   (defn geometric-series-present-value [i g n]
     (cond
       (infinite? n) (/ 1 (- i g))
       (= i g) (/ n (+ i g))
-      :else (/ (- 1 (pow (/ (+ 1 g) (+ 1 i)) n)) (- i g)))) 
-  
-  (defprotocol Monetary-Flow
-    (at [n] "get monetary flow at period n"))
-  
-  (defrecord Geometric-Series [N i Io g]
-    Monetary-Flow
-    (at [n] (if (or (<= n N) (infinite? N)) (* Io (compound-amount g n)) 0))
-    (npv [n] (* Io (if (<= n N) (- (geometric-series-present-value i g N) (geometric-series-present-value i g n)) 0))))
+      :else (/ (- 1 (pow (/ (+ 1 g) (+ 1 i)) n)) (- i g))))
   
   (rdom/render [:div "hello space"] (js/document.getElementById "app"))
   )
